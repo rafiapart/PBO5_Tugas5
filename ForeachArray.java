0e2fc26697d286ea0b10c181b18e95057ad30eb9
rafii <decik733@gmail.com>
2025-11-13 22:11:50 +0700

    no 8

---
+++ ForeachArray.java
@@ -0,0 +1,12 @@
+package Tugas5PBO;
+
+public class ForeachArray {
+    public static void main(String[] args) {
+        int[] angka = {10, 20, 30, 40, 50};
+
+        System.out.println("Menampilkan semua nilai array:");
+        for (int a : angka) {
+            System.out.println(a);
+        }
+    }
+}
