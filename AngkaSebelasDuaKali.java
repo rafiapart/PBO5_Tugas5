e43ab49675117a5368db350e5c8023121d98bfb4
rafii <decik733@gmail.com>
2025-11-13 22:07:11 +0700

    no 6

---
+++ AngkaSebelasDuaKali.java
@@ -0,0 +1,14 @@
+package Tugas5PBO;
+
+public class AngkaSebelasDuaKali {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                System.out.println(11);
+                System.out.println(11);
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
